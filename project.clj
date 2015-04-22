bb205c0cd1a7e8b0bf6d2efc20fe946a8a071b4a
Jacob Emcken <je@casalogic.dk>
2015-04-23 00:08:33 +0200

    lein ring uberjar now around 13 MB smaller

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [clj-time "0.8.0"]
-                 [jkkramer/verily "0.6.0"]
+                 [jkkramer/verily "0.6.0" :exclusions [org.clojure/clojurescript]]
                  [compojure "1.3.3"]
                  [ring/ring-defaults "0.1.4"]]
   :plugins [[lein-ring "0.9.3"]]
