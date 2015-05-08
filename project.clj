a145b90c8cef8f1b490721668ab30ba51b11e6ae
Jacob Emcken <je@casalogic.dk>
2015-05-08 23:33:19 +0200

    Bump clj-time dependency to newest stable release

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [clj-time "0.8.0"]
+                 [clj-time "0.9.0"]
                  [jkkramer/verily "0.6.0" :exclusions [org.clojure/clojurescript]]
                  [compojure "1.3.3"]
                  [ring/ring-defaults "0.1.4"]]
